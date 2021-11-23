87653f69f54d8a8557bf83911ef6621cf37df33d
Artem Archenkov <r203mail@gmail.com>
2021-11-23 16:30:43 +0400

    HW6 D

--- projects/dnd/index.js
+++ projects/dnd/index.js
@@ -15,17 +15,58 @@
    const newDiv = document.createElement('div');
    homeworkContainer.appendChild(newDiv);
  */
-import './dnd.html';
-
 const homeworkContainer = document.querySelector('#app');
 
 document.addEventListener('mousemove', (e) => {});
 
-export function createDiv() {}
+export function createDiv() {
+  const newDiv = document.createElement('div');
+  newDiv.classList.add('new-div');
+  newDiv.style.top = getRandomNumber(0, 100) + 'vh';
+  newDiv.style.left = getRandomNumber(0, 100) + 'vh';
+  newDiv.style.width = getRandomNumber(10, 100) + 'px';
+  newDiv.style.height = getRandomNumber(10, 100) + 'px';
+  newDiv.style.backgroundColor =
+    'rgb' +
+    '(' +
+    getRandomNumber(0, 255) +
+    ',' +
+    getRandomNumber(0, 255) +
+    ',' +
+    getRandomNumber(0, 255) +
+    ')';
+  newDiv.setAttribute('draggable', true);
+  return newDiv;
+}
 
 const addDivButton = homeworkContainer.querySelector('#addDiv');
 
 addDivButton.addEventListener('click', function () {
   const div = createDiv();
   homeworkContainer.appendChild(div);
+});
+
+function getRandomNumber(min, max) {
+  const random = Math.random() * (max - min) + min;
+  const roundingRandom = Math.round(random);
+  return roundingRandom;
+}
+
+let currentDrag;
+
+document.addEventListener('dragstart', (e) => {
+  currentDrag = { node: e.target };
+  e.dataTransfer.setData('text/html', 'dragstart');
+});
+
+document.addEventListener('dragover', (e) => {
+  e.preventDefault();
+  currentDrag.node.classList.add('hidden');
+});
+
+document.addEventListener('drop', (e) => {
+  e.preventDefault();
+  currentDrag.node.classList.remove('hidden');
+  currentDrag.node.style.left = e.pageX + 'px';
+  currentDrag.node.style.top = e.pageY + 'px';
 });
